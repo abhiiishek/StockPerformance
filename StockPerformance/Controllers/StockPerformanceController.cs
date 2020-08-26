bc76cd3605ba0026a0474cab2c4455dd97372e66
erp1289 <erp1289@outlook.com>
2020-08-26 17:06:25 +0530

    New Project

---
+++ StockPerformance/Controllers/StockPerformanceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+using StockPerformance.Models;
+
+namespace StockPerformance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockPerformanceController : ControllerBase
+    {
+        [HttpGet]
+        
+        public IEnumerable<StockPerformance> getStockPerformanceReport()
+        {
+            using (StreamReader r = new StreamReader("./wwwroot/assets/data/StockPerformance.json"))
+            {
+                string json = r.ReadToEnd();
+                List<StockPerformance> stockList = JsonConvert.DeserializeObject<List<StockPerformance>>(json);
+                return stockList;
+            }
+
+        }
+
+        public class StockPerformance
+        {
+            public string company { get; set; }
+            public string description { get; set; }
+            public decimal initial_price { get; set; }
+            public decimal price_2002 { get; set; }
+            public decimal price_2007 { get; set; }
+            public string symbol { get; set; }
+
+        }
+    }
+}
---
+++ StockPerformance/Models/StockPerformance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockPerformance.Models
+{
+    public class StockPerformance
+    {
+        public string company { get; set; }
+        public string description { get; set; }
+        public decimal initial_price { get; set; }
+        public decimal price_2002 { get; set; }
+        public decimal price_2007 { get; set; }
+        public string symbol { get; set; }
+
+    }
+}
